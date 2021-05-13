01e7e2b354f66007422c46c442da0d075f2180a3
phost1111 <phost1111@gmail.com>
2021-05-13 21:15:47 +0200

    Rename frontend to teemotrack_frontend

--- src/teemotrack_frontend/apps.py
+++ src/teemotrack_frontend/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FrontendConfig(AppConfig):
+class TeemotrackFrontendConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'frontend'
+    name = 'teemotrack_frontend'
